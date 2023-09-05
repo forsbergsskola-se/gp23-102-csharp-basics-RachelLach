e8ceac7178d51db7fdf959238499cf3580ff6983
RachelLach <144023790+RachelLach@users.noreply.github.com>
2023-09-05 11:35:29 +0200

    Submit P5 Variabels

--- P4Comments/Program.cs
+++ P4Comments/Program.cs
@@ -1,6 +1,6 @@
-﻿//Rachel,
-//5/9/23, 
-//rachel.lachiani@student.forsbergsskola.se
+﻿/* Rachel Lachiani
+5/9/23 
+rachel.lachiani@student.forsbergsskola.se */
 
 Console.WriteLine("Hello, World!");
 
