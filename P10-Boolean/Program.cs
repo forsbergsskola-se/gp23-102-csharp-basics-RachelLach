25f49b78fd76676c38375efbd2e45a74070344a9
RachelLach <144023790+RachelLach@users.noreply.github.com>
2023-09-07 15:16:04 +0200

    Submit: P10-Boolean

---
+++ P10-Boolean/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Channels;
+using static System.Console;
+
+WriteLine("What is your age?");
+string ageInput = ReadLine();
+double d = double.Parse(ageInput);
+
+int age = int.Parse(ageInput);
+bool ischild = age < 12;
+bool isTeenager = age >= 13 && age <= 19; 
+bool isAdult = age > 19; 
+
+WriteLine($"you are a child:{ischild}");
+WriteLine($"you are a teenager:{isTeenager}");
+WriteLine($"You are an adult:{isAdult}");
+
--- P9- Operators/Program.cs
+++ P9- Operators/Program.cs
@@ -1,27 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// Exercise 1: 
+Console.WriteLine("speed in kilometerPerHour:");
+double KilometerPerHour = double.Parse(Console.ReadLine());
+double meterPerSec = KilometerPerHour * 1000 / 60 *60;
+Console.WriteLine($"Speed in meterPerSec");
+
+
+// Exercise 2:
+Console.WriteLine("minutes");
+int Input = int.Parse(Console.ReadLine());
+int result = Input;
+int minutes = 60;
+minutes = minutes * 60;
+Console.WriteLine($"{minutes} minutes = {minutes * 60} seconds");
+
+
+// Exercise 10: 
 Console.WriteLine("Give me a number of seconds");
-string number = Console.ReadLine();
 
-static void convertSectoDay(int n)
-{
-    throw new NotImplementedException();
-}
+int seconds = Convert.ToInt32(Console.ReadLine());
+double d = Convert.ToDouble(seconds);
+float totalDays = (float) seconds / (24 * 3500);
 
-int day = 5000 / (24 * 3600);
-      
-5000 = 5000 % (24 * 3600);
-int hour = 5000 / 3600;
-      
-5000 %= 3600;
-int minutes = 5000 / 60 ;
+int days = seconds / (24 * 3600);
+seconds = seconds % (24 * 3600);
 
-var i = 5000;
-5000 %= 60;
-int seconds = 5000;
+int hours = seconds / 3600;
+seconds = seconds % 3600;
+
+minutes = seconds / 60;
+seconds = seconds % 60; 
+
+Console.WriteLine($"seconds: {seconds}");
+Console.WriteLine($"minutes: {minutes}");
+Console.WriteLine($"hours: {hours}");
+Console.WriteLine($"days: {days}");
+
+Console.WriteLine($"{days}.{hours}:{minutes}:{seconds}");
+
+Console.WriteLine($"In total, that's {totalDays} Days.");
+
+// Group exercise: 
+Console.WriteLine("?");
+string a = Console.ReadLine();
+float b = float.Parse(a);
+
+Console.WriteLine("?");
+
+
+    
+
+
+
+
+
+
+
+
+
           
-Console.WriteLine( day + " " 
-                       + "days " + hour + " " 
-                       + "hours " + minutes + " "
-                       + "minutes " + seconds + " "
-                       + "seconds ");
