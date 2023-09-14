fbbdfe38523528975670a9d7c611f891fb02265f
RachelLach <144023790+RachelLach@users.noreply.github.com>
2023-09-14 13:56:23 +0200

    Submit:P13-Go-to

--- P13-Go-to/Program.cs
+++ P13-Go-to/Program.cs
@@ -2,23 +2,23 @@
 int myNumber = random.Next(1, 101);
 
 Console.WriteLine("Welcome to my first game called Number Guessing. Are you good at guessing?");
-Console.WriteLine("I chosed a number between 1 and 100. ");
-Console.Write("Are you ready? so guess which number I chosed and enter your number");
+Console.WriteLine("I chose a number between 1 and 100. ");
+Console.Write("Are you ready? so guess which number I chose and enter your number");
 
 myNumber = 51;
 
-gussingRound:
+guessingRound:
 int inputGuess = int.Parse(Console.ReadLine());
 if (inputGuess < myNumber)
 {
     Console.WriteLine("Guess higher!");
-    goto gussingRound;
+    goto guessingRound;
 }
 Console.WriteLine("Guess another number");
 if (inputGuess > myNumber)
 {
     Console.WriteLine("Guess lower!");
-    goto gussingRound;
+    goto guessingRound;
 }
 else
 {
